bd1305ca7b4146a5b9f01de7c78a115357a0a008
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-07-27 18:34:32 +0530

    video

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let timer;
 let isPlaying = true;
 
 function updateDisplay(seconds) {
-  const mins = Math.floor(seconds / 60);
+  const mins = Math.floor(seconds / 60).toString().padStart(2, '0');
   const secs = Math.floor(seconds % 60).toString().padStart(2, '0'); // Ensure seconds are two digits
   timeDisplay.textContent = `${mins}:${secs}`;
 }
